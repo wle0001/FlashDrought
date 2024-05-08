cf16462fee4da3754857c0f04a7dda0a2c75ce38
Lee Ellenburg <ellenbw@ellenburg-mbp.local>
2024-05-08 17:09:13 -0500

    updated dm script

--- dm_FD_script.py
+++ dm_FD_script.py
@@ -27,9 +27,27 @@ from matplotlib.dates import date2num
 
 
 # hello
-direc = 'DM_data/NewClipped/*.tif'
+direc = '../DM_data/NewClipped/*.tif'
+
 yr = ''
 mo = ''
+cond = 1#123
+
+if cond == 1: #2cat/3weeks
+    period = 3
+    catchange = 1 # > cat change so 1 is a 2 or more
+    outdir = '../DM_FD_Cond{}/'.format(cond)
+    
+if cond == 2: #3cat/5weeks
+    period = 5
+    catchange = 2 # > cat change so 2 is a 3 or more
+    outdir = '../DM_FD_Cond{}/'.format(cond)
+    
+if cond == 3: #4cat/6weeks
+    period = 6
+    catchange = 3 # > cat change so 3 is a 4 or more
+    outdir = '../DM_FD_Cond{}/'.format(cond)
+    
 
 # Call correct data from folder. add \*.tif to the end of it
 tifList = glob.glob(direc)
@@ -50,15 +68,17 @@ for tifp in tifList[1:]:
     tif1 = np.append(tif1,tif, axis = 0)
     
 dfts = pd.DataFrame(range(len(dts)), index = pd.to_datetime(dts), columns = ['ids'])
-sys.exit()
+#sys.exit()
 #for season in ['autumn', 'winter', 'growing']:#'spring', 'summer',
 #for mo in range(12):
-for yr in range(2000,2023):
+#for yr in range(2000,2023):
+for season in ['all']:
     
-    season = 'year'
+    #season = 'year'
     #tif_name = 'dm_falshCount_{}.tif'.format(season)
     #tif_name = 'dm_falshCount_{}_{}.tif'.format(season,str(mo+1))
-    tif_name = 'dm_falshCount_{}_{}.tif'.format(season,str(yr))
+    tif_name = 'dm_flashCount_{}_{}.tif'.format(season,str(yr))
+    tif_name2 = 'dm_flashDrought_{}_{}.tif'.format(season,str(yr))
     
     if season == 'year':
         select = dfts[(dfts.index.year == yr)]['ids']
@@ -88,13 +108,17 @@ for yr in range(2000,2023):
     flashTif = np.empty((selectif[0].shape[0], selectif[0].shape[1]))
     flashTif[:] = np.nan
     
+    flashTif2 = np.empty(selectif.shape)
+    flashTif2[:] = np.nan
+    
+    
     
     for row in range(selectif[0].shape[0]):
         for col in range(selectif[0].shape[1]):
             dfcol = str(row)+'_'+str(col)
     
             ts = pd.Series(selectif[:,row, col], index = select.index, name = dfcol, dtype = np.int8).replace(15.0, np.nan)
-            tsd = ts.diff(periods=3)
+            tsd = ts.diff(periods=period)
     
             conds = []
             for ix, difs in enumerate(tsd):
@@ -107,7 +131,7 @@ for yr in range(2000,2023):
                 elif difs == 1:
                     val = 2   #2 = deterioration
                     conds.append(val)
-                elif difs > 1:
+                elif difs > catchange:
                     if 3 in conds[ix-4:ix]:
                         val = 0 # flash drought already counted
                     else:
@@ -123,38 +147,50 @@ for yr in range(2000,2023):
             
             count = len(conds[conds==3])
             flashTif[row,col] = count
+            flashTif2[:,row,col] = conds
     
     #sys.exit()        
-    with rio.open(tif_name, 'w', **tifmeta) as dst:
+    with rio.open(outdir+tif_name, 'w', **tifmeta) as dst:
         dst.write(np.expand_dims(flashTif,0)) 
+    #for the big tif with all data 
+    tifmeta2 = tifmeta
+    tifmeta2['count'] = flashTif2.shape[0]
+    with rio.open(outdir+tif_name2, 'w', **tifmeta2) as dst:
+        dst.write(np.expand_dims(flashTif2,0)) 
 
 #a = dfts.shape[0]
 #b = len(select)
 #c = selectif.shape
 
 
+'''
+
+#Plots
+
 
 import geopandas as gpd
 from rasterstats import zonal_stats
-states = gpd.read_file('SE_states.shp')
+states = gpd.read_file('/Users/ellenbw/Documents/UAH/SCO/NOAA_FD/Shae/SE_states.shp')
 shp = '/Users/ellenbw/Documents/UAH/SCO/LGI/HUC12_SE/SEHuc12.shp'
 gdf = gpd.read_file(shp)
 
-tif = 'dm_falshCount_autumn.tif'
+tif = '../DM_FD_Cond1/dm_flashCount_all_.tif'
 
-gdf['mean'] = pd.DataFrame(
+gdf['max'] = pd.DataFrame(
     zonal_stats(
         vectors=gdf['geometry'], 
         raster=tif, 
-        stats='mean'
+        stats='max'
     )
-)['mean']
+)['max']
 
 fig, ax = plt.subplots() 
 
-gdf.plot(column='mean', cmap='hot_r',vmax = 5,legend=True, ax = ax)
+vmax = gdf['max'].max()
+
+gdf.plot(column='max', cmap='hot_r',vmax = vmax,legend=True, ax = ax)
 states.plot(facecolor = 'none', ax= ax)
-ax.set_title('Jan')
+ax.set_title('Cond1')
 
 
 #poststamp plots
@@ -194,7 +230,7 @@ cax = fig.add_axes([0.95, 0.1, 0.03, 0.8])
 
 fig.colorbar(sm, cax = cax)
 
-
+'''
 
 
 
